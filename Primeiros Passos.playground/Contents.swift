24e33e5852a97cfed59a54c5d1e1aa40c9540085
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-05-03 00:26:23 -0300

    Arrays e Tuplas

--- Primeiros Passos.playground/Contents.swift
+++ Primeiros Passos.playground/Contents.swift
@@ -1,14 +1,57 @@
 import UIKit
 
-// Let = constante
-// Var = mutavel
+let isHungry = true
 
-let intString = "1" // string
-let pi = 3.14 // doubles
-let int = 1 // inteiros
-let bool = true // boolean
+if isHungry {
+    print("Sim")
+} else {
+    print("Não")
+}
 
-//Conversão de Dados ou Casting
-let piString = String(pi)
-let convertStrintToNumber = Int(intString)
+let company = "Google"
 
+let product = company == "Epou" ? "Android" : "iPhone"
+
+// let coords = (lat: 1.0, long: 3.1)
+
+// print(coords.lat)
+
+// let coords: (Double, Double) = (1.3, 3.3)
+
+// print(coords.0)
+
+let camera = (x: 1, y: 2, z: 3)
+
+//Destructuring
+let (x, y, z) = camera
+    
+
+var usernames: [String] = ["Guilherme"]
+
+usernames += ["Assis", "Santos"]
+
+//Fatiar
+var names = usernames[0...1]
+
+//Cria um novo array com os itens fatiados
+var namesNewArray = Array(usernames[0...2])
+
+//Adiciona
+namesNewArray.append("Teste")
+
+namesNewArray.count
+
+namesNewArray.isEmpty
+
+namesNewArray.contains("Guilherme")
+
+namesNewArray.first
+
+//Precisa nem falar
+namesNewArray.removeAll()
+
+namesNewArray.isEmpty
+
+namesNewArray.insert("Guilherme", at: <#T##Int#>)
+
+namesNewArray.remove(at: 0)
