63cb076d55cee249a80f2ecb13dd80d1e1656d64
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-05-02 23:18:15 -0300

    Tipos de Dados e conversão

--- Primeiros Passos.playground/Contents.swift
+++ Primeiros Passos.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Let = constante
+// Var = mutavel
+
+let intString = "1" // string
+let pi = 3.14 // doubles
+let int = 1 // inteiros
+let bool = true // boolean
+
+//Conversão de Dados ou Casting
+let piString = String(pi)
+let convertStrintToNumber = Int(intString)
+
